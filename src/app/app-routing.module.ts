bfd9f33561797a20a348a1d4294208fe7439fb8d
sarra touffehi <sarratouffehi70@gmail.com>
2023-05-16 22:51:47 +0200

    update color

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ErrorComponent } from './components/error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'sign-in', title:'Sign-in',component:SignInComponent},
+  {path:'sign-up', title:'Sign-up',component:SignUpComponent},
+ {path:'menu',title:'Menu',component:MenuComponent},
+ {path:'', redirectTo:'menu', pathMatch:'full'},
+ {path:'**', title:'Erreur', component:ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { Headerv2Component } from './components/headerv2/headerv2.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SignInComponent,
+    SignUpComponent,
+    MenuComponent,
+    Headerv2Component,
+    ErrorComponent,
   ],
   imports: [
     BrowserModule,
